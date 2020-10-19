90da4f903aa4527899a8ed81692250323ba2ad0f
Aaron Chambers <aaronchambers@pop-os.localdomain>
2020-10-19 08:28:21 -0700

    added frame support

--- SSD1305_save.lua
+++ SSD1305_save.lua
@@ -35,34 +35,47 @@ end
 width = sprite.width
 height = sprite.height
 
--- image data
 local fsprite = Sprite(sprite)
-local fcel = sprite.cels[1]
-local fimage = fcel.image
+
 fsprite:flatten()
 
+-- meta data
+file:write(string.char(0))
 file:write(string.char(width))
 file:write(string.char(height))
 
-local zeroPadding = width-((width/8)*8)
+local frames = 0
+for c, cel in ipairs(fsprite.cels) do
+    frames = frames + 1
+end
+
+file:write(string.char(frames))
+
+-- image data
+local zeroPadding = fsprite.width-((fsprite.width/8)*8)
 local bytes = width/8;
-for y = 0, fsprite.height - 1 do
-    for b = 0, bytes - 1 do
-        local byte = 0
-        for x = 0, 7 do
-            local px = app.pixelColor.grayaV(fimage:getPixel(x + b * 8, y))
-            if px > 1 then
-                px = 1
-            else
+for c, cel in ipairs(fsprite.cels) do
+    app.alert(c)
+    local fimage = cel.image
+    for y = 0, fsprite.height - 1 do
+        for b = 0, bytes - 1 do
+            local byte = 0
+            for x = 0, 7 do
+                local px = app.pixelColor.grayaV(fimage:getPixel(x + b * 8, y))
+                if px > 1 then
+                    px = 1
+                else
+                    px = 0
+                end
+                
+                byte = byte | (px << x)
                 px = 0
             end
-            
-            byte = byte | (px << x)
-            px = 0
+            file:write(string.char(byte))
         end
-        file:write(string.char(byte))
     end
 end
+
 file:close()
 
 app.alert("SSD1305 file saved as " .. filename)
